f0a5a871a39b929951a14730dffd93f433a2d50c
Bernard Arhia <bernardarhia@gmail.com>
2020-11-23 22:48:39 +0000

    added icons

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -11,7 +11,16 @@ const Favorite = ({sidebarOpen, setSidebarOpen, darkMode}) => {
            background:darkMode ? '#fff':'#000'
         }}>
             <div className="close-favorite" onClick={handleSidebar}>
-                <RiCloseLine fill={darkMode ? ' #797979':' #fff'} />
+                <RiCloseLine fill={darkMode ? '#797979':' #fff'} />
+            </div>
+            <h1 style={{color:darkMode ? ' #36434d':'#fff', textAlign:"center",margin:'1rem 0'}}>Favorites</h1>
+
+            <div className="favorite__lists">
+                <ul>
+                    {[1,2,3,4,5,6,7,8,9,10,11,12,131,23,134,3546,45657,35,6535,68,3535].map((list)=>{
+                        return <li key={list}>Item {list}</li>
+                    })}
+                </ul>
             </div>
         </div>
     )
