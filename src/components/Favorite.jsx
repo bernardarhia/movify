b3862afbb323522943692aea3a40f4cb8eec5212
Bernard Arhia <bernardarhia@gmail.com>
2020-11-24 17:20:01 +0000

    added favorite component and all the neccessary functions

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,29 +1,101 @@
-import React from 'react'
-import {RiCloseLine} from 'react-icons/ri';
-const Favorite = ({sidebarOpen, setSidebarOpen, darkMode}) => {
-    const handleSidebar = ()=>{
-        setSidebarOpen(!sidebarOpen);
-      }
-    return (
-        <div className="favorite-container" 
+import React, { useState, useEffect } from "react";
+import { RiCloseLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import { VscChromeClose } from "react-icons/vsc";
+const Favorite = ({ sidebarOpen, setSidebarOpen, darkMode }) => {
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [favoriteLoading, setFavoriteLoading] = useState(false);
+
+  //  sidebar toggling
+  const handleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  const deleteFavorite = (item) => {
+    const getFavoriteMovies = JSON.parse(
+      localStorage.getItem("favoriteMovies")
+    );
+    const newFavorite = getFavoriteMovies.filter((data) => data.id !== item.id);
+    setFavoriteMovies(newFavorite);
+    localStorage.setItem("favoriteMovies", JSON.stringify(newFavorite));
+  };
+  useEffect(() => {
+    const getFavoriteMovies = JSON.parse(
+      localStorage.getItem("favoriteMovies")
+    );
+    // get favorite movies
+    if (!getFavoriteMovies)
+      localStorage.setItem("favoriteMovies", JSON.stringify([]));
+
+    setInterval(() => {
+      const retrieved = localStorage.getItem("favoriteMovies");
+      setFavoriteMovies(JSON.parse(retrieved));
+    }, 1000);
+  }, [setFavoriteMovies]);
+
+  return (
+    <div
+      className="favorite-container"
+      style={{
+        right: sidebarOpen ? "0" : "-200%",
+        background: darkMode ? "#fff" : "#000",
+      }}
+    >
+      <div className="close-favorite" onClick={handleSidebar}>
+        <RiCloseLine fill={darkMode ? "#797979" : " #fff"} />
+      </div>
+      <h1
         style={{
-           right: sidebarOpen ? '0':'-200%',
-           background:darkMode ? '#fff':'#000'
-        }}>
-            <div className="close-favorite" onClick={handleSidebar}>
-                <RiCloseLine fill={darkMode ? '#797979':' #fff'} />
-            </div>
-            <h1 style={{color:darkMode ? ' #36434d':'#fff', textAlign:"center",margin:'1rem 0'}}>Favorites</h1>
+          color: darkMode ? " #36434d" : "#fff",
+          textAlign: "center",
+          margin: "1rem 0",
+        }}
+      >
+        Favorites
+      </h1>
 
-            <div className="favorite__lists">
-                <ul>
-                    {[1,2,3,4,5,6,7,8,9,10,11,12,131,23,134,3546,45657,35,6535,68,3535].map((list)=>{
-                        return <li key={list}>Item {list}</li>
-                    })}
-                </ul>
-            </div>
-        </div>
-    )
-}
+      <div className="favorite__lists">
+        {favoriteMovies.length === 0 && (
+          <h1 className="head" style={{ textAlign: "center" }}>
+            No favorites found
+          </h1>
+        )}
+        {favoriteMovies.length > 0 && (
+          <ul>
+            <p
+              onClick={() =>
+                localStorage.setItem("favoriteMovies", JSON.stringify([]))
+              }
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                color: darkMode ? "#333" : "#fff",
+                fontWeight: "600",
+              }}
+            >
+              Remove all
+            </p>
+            {favoriteMovies.map((movie, index) => {
+              return (
+                <li key={movie.id}>
+                  <div className="img">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                      alt=""
+                    />
+                  </div>
+                  <Link to="">{movie.original_title}</Link>
+                  <div className="del" onClick={() => deleteFavorite(movie)}>
+                    <VscChromeClose />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Favorite
+export default Favorite;
--- src/components/ShowMovie.jsx
+++ src/components/ShowMovie.jsx
@@ -2,6 +2,29 @@ import React from "react";
 import { RiHeartFill, RiPlayFill } from "react-icons/ri";
 
 const ShowMovie = ({ movies }) => {
+  const addToFavorite = async (item)=>{
+
+  // check if local storage has it already
+  const getFavorite = JSON.parse(localStorage.getItem('favoriteMovies'))
+  if(!getFavorite)localStorage.setItem('favoriteMovies',JSON.stringify([item]));
+
+    
+    const newData = JSON.parse(localStorage.getItem('favoriteMovies'));
+    
+    let idArrays = [];
+    newData.map((data)=>{
+         idArrays.push(data.id);
+    })
+  if(idArrays.includes(item.id)){
+    alert(`${item.original_title} is already in favorites`)
+    return;
+  }else{
+    const newItem = [...newData,item]
+    localStorage.setItem('favoriteMovies',JSON.stringify(newItem));
+  }
+
+  
+  }
   return (
     <>
       <div className="rated">
@@ -34,8 +57,8 @@ const ShowMovie = ({ movies }) => {
                       <RiPlayFill />
                     </div>
                     <div className="fav">
-                      <input type="checkbox" id={`check-${index}`} />
-                      <label htmlFor={`check-${index}`}>
+                      <input type="checkbox" id={`check-${item.id}`} />
+                      <label htmlFor={`check-${item.id}`} onClick={()=>addToFavorite(item)}>
                         <RiHeartFill />
                       </label>
                     </div>
